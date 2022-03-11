7c1fdd817c330ca131b346f1e2fe930b1736f4ed
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-11 18:38:51 +0300

    adding the application bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: ,
+      ),
     ),
   );
 }
