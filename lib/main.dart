c56d8c8c1d2cb9d1eb30d750d89ed0e2f8ba35e4
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-11 19:06:48 +0300

    updating yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,13 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I Am Crazy'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(image: image),
         ),
       ),
     ),
